64225b98b8518ab7c66eca70c68b754993b13893
Karen Largent <karenlargent@Karens-MacBook-Pro.local>
2019-06-05 16:29:49 -0400

    close to final, needs some buig fixes in CSS

--- assets/gifGenerator.js
+++ assets/gifGenerator.js
@@ -49,7 +49,7 @@ function GIFgenerator () {
         // for loop going through the results of the response 
         for (var i = 0; i < searchResults.length; i++) {
             // create a new div for each gif
-            var gifDiv = $("<div>");
+            var gifDiv = $("<div col-sm>");
             
             // each gif is an image 
             var gif = $("<img class= 'gif' data-state ='animate'>");
